836c6d5dca3b138a11c73ed868e43eddf3241e04
menekratis <menekratis@gmail.com>
2026-05-26 13:34:58 +0300

    more string testing

--- strings/strings.js
+++ strings/strings.js
@@ -1,5 +1,6 @@
 
-// Including an apostrophe in a string can be done by escaping it with a backslash (\)  
+// Including an apostrophe in a string 
+// can be done by escaping it with a backslash (\)  
 const bigmouth = 'I\'ve got no right to take my place…';
 console.log(bigmouth);
 
@@ -32,7 +33,9 @@ console.log(char); // "A"
 let code = text.codePointAt(0);
 console.log(code); // 65
 
-//The at() method returns the character at a specified index (position) in a string.
+//The at() method returns 
+// the character at a specified index (position) in a string.
+
 const str = "Hello, World!";
 let letter = str.at(2); // "l"
 console.log(letter); 
@@ -41,3 +44,68 @@ console.log(letter);
 let proper = "hello world";
 let acc = proper[0];
 console.log(acc); // "h"
+
+//concat() joins two or more strings:
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3); // "Hello World"
+
+//The slice() method extracts a part of a string 
+// and returns the extracted part in a new string.
+
+let txt = "Apple, Banana, Kiwi";
+let part = txt.slice(7, 13);
+console.log(part); // "Banana"
+
+//If you omit the second parameter, 
+// the method will slice out the rest of the string: 
+
+let part2 = txt.slice(7);
+console.log(part2); // "Banana, Kiwi"
+
+let part3 = txt.slice(-12, -6);
+console.log(part3); // "Banana"
+
+let part4 = txt.slice(-12);
+console.log(part4); // "Banana, Kiwi"
+
+//The split() method splits a string into an array of substrings, 
+// and returns the new array.
+
+let txt2 = "Hello World";
+let arr = txt2.split(" ");
+console.log(arr); // ["Hello", "World"]
+
+//substr() is similar to slice(). 
+// The difference is that the second parameter 
+// specifies the length of the extracted part, not the end position.
+
+let str2 = "Apple, Banana, Kiwi";
+let arr2 = str2.substr(7,6);
+console.log(arr2); // "Banana" 
+
+let arr3 = str2.substr(7);
+console.log(arr3); // "Banana, Kiwi"
+
+let arr4 = str2.substr(-4);
+console.log(arr4); // "Kiwi"
+
+//The toUpperCase() method converts a string to uppercase letters.
+
+let txt3 = "Hello World";
+let upper = txt3.toUpperCase();
+console.log(upper); // "HELLO WORLD"
+
+//The toLowerCase() method converts a string to lowercase letters.
+
+let lower = txt3.toLowerCase();
+console.log(lower); // "hello world"
+
+//The trim() method removes whitespace from both sides of a string.
+
+let txt4 = "   Hello World!   ";
+let trimmed = txt4.trim();
+console.log(trimmed); // "Hello World!" 
+
